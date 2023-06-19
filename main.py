71caeb0d86fa25cf446c68cfef9364aa07c328c9
Nodazi <dk3146@naver.com>
2023-06-19 11:28:50 +0900

    링크에서 테이블에 담긴 값 불러오는 기능 추가

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-# This is a sample Python script.
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+import requests
+from bs4 import BeautifulSoup
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+url = "https://testmoa.com/korea-mbti-statistics/#hangug_MBTI_biyul"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.select('#post-3691 > div > div.dynamic-entry-content > figure > table > tbody >tr')
+
+    for row in title:
+        text = row.get_text(' ', strip=True)
+        print(text)
+
+else:
+    print(response.status_code)
