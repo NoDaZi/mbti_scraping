7b3c07178a0196b3a862202a628eddfcb1d08b3a
Nodazi <dk3146@naver.com>
2023-06-19 15:43:09 +0900

    re 모듈을 이용하여 인원수 slicing mbti 리스트에 (mbti, 인원수) 튜플 형태로 저장

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = "https://testmoa.com/korea-mbti-statistics/#hangug_MBTI_biyul"
 
 response = requests.get(url)
+
+mbti = []
 
 if response.status_code == 200:
     html = response.text
@@ -11,8 +14,10 @@ if response.status_code == 200:
     title = soup.select('#post-3691 > div > div.dynamic-entry-content > figure > table > tbody >tr')
 
     for row in title:
-        text = row.get_text(' ', strip=True)
-        print(text)
+        text = row.get_text(' ', strip=True).split()
+        mbti.append((text[1], re.sub(r'[^0-9]', '', text[-1])))
 
 else:
     print(response.status_code)
+
+print(mbti)
